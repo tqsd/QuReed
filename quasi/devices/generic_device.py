537b56cb16f289ba28e0a08c1175c59010041ca0
SimonSekavcnik <simon.sekavcnik@gmail.com>
2023-04-27 00:02:29 +0200

    Defined structure for signals

--- quasi/devices/generic_device.py
+++ quasi/devices/generic_device.py
@@ -1,10 +1,17 @@
 """ Generic Device definition """
 from abc import ABC, abstractmethod
+from typing import Type
+
+from quasi.signals.generic_signal import GenericSignal
 
 class GenericDevice(ABC):
     """
     Generic Device class used to implement every device
     """
+    @abstractmethod
+    def __init__(self, inputs:Type[GenericSignal], outputs:Type[GenericSignal]):
+        self.inputs = inputs
+        self.outputs = outputs
 
     @abstractmethod
     def compute_outputs(self):
---
+++ quasi/signals/generic_signal.py
@@ -0,0 +1,22 @@
+"""
+Generic signal implementation,
+used for defining inputs and outputs
+"""
+from abc import ABC, abstractmethod
+
+class GenericSignal(ABC):
+    """
+    Generic Signal class used to implement any other signal class
+    """
+
+    @abstractmethod
+    def __init__(self):
+        pass
+
+
+    @abstractmethod
+    def dummy(self):
+        """
+        Dummy method to appease the linters.
+        """
+        pass
