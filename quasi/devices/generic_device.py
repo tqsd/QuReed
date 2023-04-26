0c7ad1228af67aa970fec5cc08982cf0b6e582e1
SimonSekavcnik <simon.sekavcnik@gmail.com>
2023-04-26 23:46:40 +0200

    compute_outputs method

--- quasi/devices/generic_device.py
+++ quasi/devices/generic_device.py
@@ -5,3 +5,12 @@ class GenericDevice(ABC):
     """
     Generic Device class used to implement every device
     """
+
+    @abstractmethod
+    def compute_outputs(self):
+        """
+        Computes all outputs given the inputs
+        It should rise an exception if the inputs were not
+        yet computed.
+        """
+        pass
