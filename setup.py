62a5da8dd4894bbf263982c3cac44a8e40223d5f
SimonSekavcnik <simon.sekavcnik@gmail.com>
2023-04-26 22:07:46 +0200

    Added setup.py, .gitignore and updated README

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='quasi',
+    version='0.0.1',
+    author="Simon Sekavčnik",
+    author_email="simon.sekavcnik@tum.de",
+    description="Simulating quantum experiments with realistic device models",
+    license="Apache 2.0",
+    packages=["quasi"],
+    install_requires=["numpy"]
+)
