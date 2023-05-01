71e41dd254a10456b3093bc5c0b0cce79bf02c34
JanisCGN <45869180+JanisCGN@users.noreply.github.com>
2023-05-01 20:55:50 +0200

    Update beam_splitter.py
    Added example for how to include reference (just use bibtex entry of paper associated to device)
    Raised question how to add physical units?

--- quasi/devices/beam_splitter.py
+++ quasi/devices/beam_splitter.py
@@ -12,7 +12,21 @@ class BeamSplitter(GenericDevice):
     """
     power_average = 0
     power_peak = 0
-    reference = None
+    bandwidth = 100 MHz # how do we add physical units??
+    reference = {Deng21, 
+                    author = {Chunyu Deng and Mengjia Lu and Yu Sun and Lei Huang and Dongyu Wang and Guohua Hu and Ruohu Zhang and Binfeng Yun and Yiping Cui},
+                    journal = {Opt. Express},
+                    keywords = {Coherent communications; Effective refractive index; Extinction ratios; Numerical simulation; Phase shift; Polarization splitters},
+                    number = {8},
+                    pages = {11627--11634},
+                    publisher = {Optica Publishing Group},
+                    title = {Broadband and compact polarization beam splitter in LNOI hetero-anisotropic metamaterials},
+                    volume = {29},
+                    month = {Apr},
+                    year = {2021},
+                    url = {https://opg.optica.org/oe/abstract.cfm?URI=oe-29-8-11627},
+                    doi = {10.1364/OE.421262},
+                }
     ports = {
         "A": Port(label="A", direction="input",signal=None,
                   signal_type=GenericQuantumSignal, device=None),
