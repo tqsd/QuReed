9e0b5773dfc8a26d59f31c30a4648948a344f500
Kareem H. El-Safty <kareemessafty@gmail.com>
2023-07-18 14:59:20 +0200

    bibtext generation. defining new component

--- examples/fock_ops.py
+++ examples/fock_ops.py
@@ -1,14 +1,20 @@
 from quasi.components.gates import Beamsplitter, Displacment, Phase, Squeezing
 from quasi._math.fock import vacuum_state, mean_photon_number, fock_probability
-
+from quasi.components.gates import Components
+import numpy as np
 
 qumode = 1
 cutoff = 10
 init_state = vacuum_state(n=qumode, cutoff=cutoff)
 
-displacement_operator = Displacment(1, 0.5, cutoff=10)
+user_defined_component = Components(cutoff=cutoff)
+user_defined_component.matrix = np.identity(n=10)
+
+displacement_operator = Displacment(1, 0.5, cutoff=10).matrix
 
 displaced_state = displacement_operator @ init_state
+
+displaced_state_unphased = Phase(-0.5, cutoff=10).matrix @ displaced_state
 
 assert mean_photon_number(state=displaced_state) == 1**2
 
--- quasi/components/gates.py
+++ quasi/components/gates.py
@@ -1,3 +1,7 @@
+from __future__ import unicode_literals, print_function, absolute_import
+from builtins import str
+import requests
+
 from abc import ABC
 from quasi._math.fock import displacment, beamsplitter, phase, squeezing
 
@@ -10,48 +14,147 @@ class Components(ABC):
     signals.
     """
 
-    def get_operator(self):
+    def __init__(
+        self,
+        cutoff: int,
+        physical_description: dict = None,
+        math_eqation: str = None,
+        doi: str = None,
+        author_name: str = None,
+    ) -> None:
+        self.cutoff = cutoff
+        self.physical_description = physical_description
+        self.math_equation = math_eqation
+        self.doi = doi
+        self.matrix = None
+        self.author_name = None
+
+    def physical_properties(self, physical_description):
+        self.frequency = physical_description["frequency"]
+        self.gaussian = physical_description["gaussianity"]
+        self.passive = physical_description["passive"]
+        self.name = physical_description["name"]
+        """
+        many parameters to set here
+
+        """
+
+    def get_operator(self, math_equation):
         raise NotImplementedError
+
+    def generate_bibtex(self, doi):
+        bare_url = "http://api.crossref.org/"
+        url = "{}works/{}/transform/application/x-bibtex"
+        url = url.format(bare_url, doi)
+
+        r = requests.get(url)
+        found = False if r.status_code != 200 else True
+        bib = r.content
+
+        bib = str(bib, "utf-8")
+        if self.author_name is not None:
+            print("name allocation is not implpemente yet in this case")
+            raise NotImplementedError  # we should think of how to include author
+            # name inside the bibtex
+        return found, bib
 
 
 class Beamsplitter(Components):
-    def __init__(self, theta, phi, cutoff) -> None:
+    def __init__(
+        self,
+        theta,
+        phi,
+        cutoff,
+        physical_description: dict = None,
+        math_eqation: str = None,
+        doi: str = None,
+    ) -> None:
         self.theta = theta
         self.phi = phi
         self.cutoff = cutoff
+        self.matrix = None
         super().__init__()
 
-    def get_operator(self):
-        return beamsplitter(self.theta, self.phi, self.cutoff)
+    def get_operator(self, math_equation):
+        if math_equation is None:
+            self.matrix = beamsplitter(self.theta, self.phi, self.cutoff)
+        else:
+            raise NotImplementedError
+
+    def generate_bibtex(self, doi):
+        return super().generate_bibtex(doi)
 
 
 class Displacment(Components):
-    def __init__(self, r, phi, cutoff) -> None:
+    def __init__(
+        self,
+        r,
+        phi,
+        cutoff,
+        physical_description: dict = None,
+        math_eqation: str = None,
+        doi: str = None,
+    ) -> None:
         self.theta = r
         self.phi = phi
         self.cutoff = cutoff
+        self.matrix
         super().__init__()
 
-    def get_operator(self):
-        return displacment(self.r, self.phi, self.cutoff)
+    def get_operator(self, math_equation):
+        if math_equation is None:
+            self.matrix = displacment(self.theta, self.phi, self.cutoff)
+        else:
+            raise NotImplementedError
+
+    def generate_bibtex(self, doi):
+        return super().generate_bibtex(doi)
 
 
 class Squeezing(Components):
-    def __init__(self, r, phi, cutoff) -> None:
+    def __init__(
+        self,
+        r,
+        phi,
+        cutoff,
+        physical_description: dict = None,
+        math_eqation: str = None,
+        doi: str = None,
+    ) -> None:
         self.theta = r
         self.phi = phi
         self.cutoff = cutoff
         super().__init__()
 
-    def get_operator(self):
-        return squeezing(self.r, self.phi, self.cutoff)
+    def get_operator(self, math_equation):
+        if math_equation is None:
+            self.matrix = squeezing(self.theta, self.phi, self.cutoff)
+        else:
+            raise NotImplementedError
+
+    def generate_bibtex(self, doi):
+        return super().generate_bibtex(doi)
 
 
 class Phase(Components):
-    def __init__(self, phi, cutoff) -> None:
+    def __init__(
+        self,
+        phi,
+        cutoff,
+        physical_description: dict = None,
+        math_eqation: str = None,
+        doi: str = None,
+    ) -> None:
         self.phi = phi
         self.cutoff = cutoff
+        self.matrix = None
         super().__init__()
 
-    def get_operator(self):
-        return phase()(self.phi, self.cutoff)
+    def get_operator(self, math_equation):
+        if math_equation is None:
+            self.matrix = phase(self.theta, self.phi, self.cutoff)
+        else:
+            raise NotImplementedError
+
+    def generate_bibtex(self, doi):
+        return super().generate_bibtex(doi)
