a6993cb89fe55f130902b14589a88f7d6b5f0cd0
Simon Sekavcnik <simon.sekavcnik@gmail.com>
2024-04-15 23:46:09 +0200

    Started to work on the example for TUD

---
+++ examples/time_synchronization/custom_components/entangled_photon_source.py
@@ -0,0 +1,100 @@
+"""
+ created with template
+"""
+import mpmath
+from typing import Union
+from quasi.devices import (GenericDevice,
+                           wait_input_compute,
+                           schedule_next_event,
+                           coordinate_gui,
+                           log_action,
+                           ensure_output_compute)
+from quasi.devices.port import Port
+from quasi.simulation import ModeManager
+from quasi.signals import *
+from photon_weave.state.envelope import Envelope
+from photon_weave.state.composite_envelope import CompositeEnvelope
+from photon_weave.operation.polarization_operations import (
+    PolarizationOperation, PolarizationOperationType
+)
+from photon_weave.operation.fock_operation import (
+    FockOperationType, FockOperation
+)
+from photon_weave.operation.composite_operation import (
+    CompositeOperation, CompositeOperationType
+)
+
+class EntangledPhotonSource(GenericDevice):
+    ports = {
+        "trigger": Port(
+            label="trigger",
+            direction="input",
+            signal=None,
+            signal_type=GenericBoolSignal,
+            device=None),
+        "output": Port(
+            label="output",
+            direction="output",  # Changed from "input" to "output" for clarity
+            signal=None,
+            signal_type=GenericQuantumSignal,
+            device=None),
+    }
+
+    gui_icon = None
+    gui_tags = None
+    gui_documentation= None
+      
+    power_peak = 0
+    power_average = 0
+    
+    reference = None
+      
+    @ensure_output_compute
+    @coordinate_gui
+    @wait_input_compute
+    def compute_outputs(self, *args, **kwargs):
+        """
+        Implement to use the regular backend
+        """
+        pass
+      
+    @log_action
+    @schedule_next_event
+    def des(self, time, *args, **kwargs):
+        """
+        Implement to use discrete event simulation
+        """
+        # biexciton
+        env1 =  Envelope(wavelength=830)
+        # exciton
+        env2 =  Envelope(wavelength=820)
+        # Modify Value
+        photon_num = 5
+        a_dagger = FockOperation(FockOperationType.Creation, apply_count=photon_num)
+
+        env1.apply_operation(a_dagger)
+        env2.apply_operation(a_dagger)
+
+        H = PolarizationOperation(operation=PolarizationOperationType.H)
+        env1.apply_operation(H)
+        CNOT = CompositeOperation(CompositeOperationType.CNOT)
+        c = CompositeEnvelope(env1, env2)
+        c.apply_operation(CNOT, env1, env2)
+        # Time after the biexciton decays
+        t1 = mpmath.mpf("0.0000000000001")
+        t2 = mpmath.mpf("0.0000000000002")
+        s1 = GenericQuantumSignal()
+        s1.set_contents(content=env1)
+        s2 = GenericQuantumSignal()
+        s2.set_contents(content=env2)
+        return [("output", s1, time+t1),("output", s2, time+t2)]
+        
+
+
+    @log_action
+    @schedule_next_event
+    def des_action(self, time=None, *args, **kwargs):
+        """
+	    Or implement this if you are implementing a trigger
+	    """
+        pass
---
+++ examples/time_synchronization/main.py
@@ -0,0 +1,54 @@
+# General libraries
+import os
+import sys
+import mpmath
+# Quasi elements
+from quasi.simulation import Simulation
+from quasi.devices.control import SimpleTrigger
+from quasi.signals import (
+    GenericBoolSignal, GenericQuantumSignal)
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from time_synchronization.custom_components.entangled_photon_source import EntangledPhotonSource
+from time_synchronization.custom_components.custom_fiber import CustomFiber
+from time_synchronization.custom_components.custom_filter import CustomFilter
+
+# Helper function for connecting
+def connect(signal_type, device, port, device2=None, port2=None):
+    signal = signal_type()
+    device.register_signal(signal, port)
+    if device2 is not None and port2 is not None:
+        device2.register_signal(signal, port2)
+    return signal
+
+signals = {}
+
+# Devices
+st = SimpleTrigger(time=mpmath.mpf('0.0000000000001'), name="Trigger")
+source = EntangledPhotonSource(name="Source")
+fiber1 = CustomFiber("Fiber1", 1)
+fltr = CustomFilter("Filter")
+
+
+# Connecting the devices
+signals["1"] = connect(
+    GenericBoolSignal, st, "trigger",
+    source, "trigger"
+)
+signals["2"] = connect(
+    GenericQuantumSignal, source, "output",
+    fiber1, "input"
+)
+signals["3"] = connect(
+    GenericQuantumSignal, fiber1, "output",
+    fltr, "input"
+)
+
+
+
+
+# Running the Simulation
+simulation = Simulation.get_instance()
+simulation.run_des(1)
