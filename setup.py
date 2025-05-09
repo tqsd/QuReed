bfe2ba92b57dd6b228d50bdc8d2a35b70002d1da
Simon Sekavčnik <simon.sekavcnik@gmail.com>
2025-05-09 23:34:21 +0200

    Update setup.py
    use the latest photon_weave on PYPI

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     license="Apache 2.0",
     packages=find_packages(where="."),
     install_requires=[
-        "photon_weave==0.1.4",
+        "photon_weave",
         "jinja2",
     ],
     package_data={
